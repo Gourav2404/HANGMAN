220d622b02c1127ebe1f5554aacd5770aa08fc89
Gourav Kadam <gouravkadam49@gmail.com>
2021-09-03 20:53:41 +0530

    pushing all files

---
+++ words_hangman.py
@@ -0,0 +1,3 @@
+words = ['avenue ', 'awkward', 'banjo', 'beekeeper', 'buffalo', 'cycle', 'equip', 'fishhook', 'funny', 'gang', 'hero',
+         'icebox', 'jackpot' , 'keyhole' , 'lucky' , 'matrix' , 'nightclub' , 'numbskull' , 'nowadays' ,'puppy', 'oxygen' 
+        , 'pixel' , 'quiz' , 'rhythm' , 'scratch' , 'transplant' , 'unzip' , 'vodka' , 'wizard' , 'xylophone' , 'youthful' , 'zigzag' , 'zombie']
